590ae91d98ce18a9fb3ea8188efa636d5a044e5c
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-11-30 09:47:34 +0100

    Botones activities

--- app/src/main/java/com/example/mihorario/MainActivity.java
+++ app/src/main/java/com/example/mihorario/MainActivity.java
@@ -2,16 +2,41 @@ package com.example.mihorario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btNuevaClase
+        Button nuevaClase = findViewById(R.id.bt_anadirClase);
+        Button verHorario = findViewById(R.id.bt_verHorario);
+        Button queToca = findViewById(R.id.bt_queToca);
+    }
+
+    public void nuevaClase(View view){
+        Intent intentNuevaClase = new Intent(this, activity_anadirClase.class);
+        startActivity(intentNuevaClase);
+    }
+
+    public void verHorario(View view){
+        Intent intentVerHorario = new Intent(this, activity_verHorario.class);
+        startActivity(intentVerHorario);
+    }
+
+    public void queToca(View view){
+        Intent intentQueToca = new Intent(this, activity_queToca.class);
+        startActivity(intentQueToca);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        
     }
 }

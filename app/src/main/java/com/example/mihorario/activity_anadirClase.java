34b9b30a67350e76d33d2a9fe8370cdece4f2d48
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-11-30 09:16:09 +0100

    main layout

---
+++ app/src/main/java/com/example/mihorario/activity_anadirClase.java
@@ -0,0 +1,14 @@
+package com.example.mihorario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_anadirClase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anadir_clase);
+    }
+}
